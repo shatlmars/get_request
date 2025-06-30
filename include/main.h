f800d088b145806bef0e6e48b66d40e17a5cf215
Shadbek <shadbek87@gmail.com>
2025-07-01 02:58:38 +0300

    unpack ip addr

--- include/main.h
+++ include/main.h
@@ -3,3 +3,5 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <fstream>
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "main.h"
 
+struct sockaddr_in sa;
+
+
 
 int main(int argc, char* argv[]){
-    std::cout << "Hello world123";
+    inet_pton(AF_INET, "10.12.110.57", &(sa.sin_addr));
+    char ip4[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(sa.sin_addr), ip4, INET_ADDRSTRLEN);
+    std::cout << ip4;
     return 0;
 }
